a632a0fb555ce4ae63e313048a63e94962b21da0
64 <1453539684@qq.com>
2019-07-22 13:55:28 +0800

    home页面

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.scss";
+import React,{Component} from "react";
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import {Provider} from 'react-redux'
+import store from './store'
+import Home from './page/home'
+import Detail from './page/detail'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <BrowserRouter>
+        <div>
+           <Switch>
+              <Route path='/detail' component={Detail} />
+              <Route path='/' component={Home} />
+            </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
-
 export default App;
---
+++ src/page/home/components/Bestsellers.js
@@ -0,0 +1,53 @@
+import React,{Component} from 'react';
+import '../style.scss';
+import '../../../statics/font_1307227_9qrecju4ty/iconfont.css';
+
+class Bestsellers extends Component{
+    render(){
+      return (
+          <div>
+            <div className='title-header'>
+              <i className='iconfont icon-EnergyResearch'></i>
+              <h3 className='home-title'>销量极好榜</h3>
+            </div>
+            <div className= 'new-one'>
+              <div className='new-left'>
+                <img src="http://img-tailor.11222.cn/bcv/big/1147397894049.jpg" alt=""/>
+                <span className='new-num'>1</span>
+              </div>
+              
+              <div className='new-right'>
+                <h4 className='new-title'>绝世神皇</h4>
+                <span className='new-author'>自逍客</span>
+                <p className='book-item-desc new-desc'>废物出身，家族凤梅很大很</p>
+                <div className='book-item-tag new-tags'><span className='tag-spe'>连载</span><span>160万</span><span className='tag-red'>连载</span><span>连载</span></div>
+              </div>
+
+            </div>
+            <ul className='new-ul'>
+              <li className='new-li'>
+                <i>2</i>
+                <h3>重生之潇洒走一回</h3>
+                <div className='book-item-tag '><span className='tag-spe'>连载</span><span>160万</span></div>
+              </li>
+              <li className='new-li'>
+                <i>3</i>
+                <h3>重生之潇洒走一回</h3>
+                <div className='book-item-tag '><span className='tag-spe'>连载</span><span>160万</span></div>
+              </li>
+              <li className='new-li'>
+                <i>4</i>
+                <h3>重生之潇洒走一回</h3>
+                <div className='book-item-tag '><span className='tag-spe'>连载</span><span>160万</span></div>
+              </li>
+            </ul>
+            <div className='home-change'>
+            <span>查看更多</span>
+          </div>
+          <div className='white-space'></div>
+            </div>
+      )
+  }
+  }
+  
+  export default Bestsellers
