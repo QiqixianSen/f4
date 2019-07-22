15a0090be1c30dae04c8ab216b923d1333626f60
xiaoqi <2284925463@qq.com>
2019-07-22 22:22:12 +0800

    登录注册静态完成

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import { Provider } from "react-redux";
 import Home from "./page/home";
 import Detail from "./page/detail";
 import Login from "./page/login";
+import Register from "./page/register";
 
 class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Switch>
-                        <Route path="/login" component={Login} />
-                        <Route path="/detail" component={Detail} />
-                        <Route path="/" component={Home} />
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/detail" component={Detail} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 export default App;
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -2,24 +2,35 @@ import React, { Component } from "react";
 import { NavBar, Icon } from "antd-mobile";
 
 export default class index extends Component {
-    render() {
-        return (
-            <div>
-                <NavBar
-                    mode="light"
-                    icon={<Icon type="left" size="lg" />}
-                    onLeftClick={() => console.log("onLeftClick")}
-                    rightContent={[
-                        <Icon
-                            key="0"
-                            type="cross-circle"
-                            style={{ marginRight: "16px" }}
-                        />
-                    ]}
-                >
-                    {this.props.children}
-                </NavBar>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <NavBar
+          mode="light"
+          icon={
+            <Icon
+              type="left"
+              size="lg"
+              onClick={() => {
+                this.props.history.go(-1);
+              }}
+            />
+          }
+          onLeftClick={() => console.log("onLeftClick")}
+          rightContent={[
+            <Icon
+              key="0"
+              type="cross-circle"
+              style={{ marginRight: "16px" }}
+              onClick={() => {
+                this.props.history.push("/home");
+              }}
+            />
+          ]}
+        >
+          {this.props.children}
+        </NavBar>
+      </div>
+    );
+  }
 }
