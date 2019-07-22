ce6a81d0d10ad5eee66e41fdcee2f6be49a974de
chaot <111111111@qq.com>
2019-07-22 22:08:59 +0800

    chao

--- src/page/list/booklist/index.js
+++ src/page/list/booklist/index.js
@@ -117,6 +117,31 @@ class Booklist extends Component {
                 </div>
               </div>
             </li>
+            <li>
+              <div className="textside">
+                <div className="textmain">
+                  <h3>腐女们看过来【最近的新文】</h3>
+                  <p>都是很好看的文！希望大家喜欢都是很好看的文！希望大家喜欢</p>
+                  <div>
+                    <span>重生</span>
+                    <span>重生</span>
+                    <span>重生</span>
+                    <span>娱乐圈</span>
+                  </div>
+                </div>
+                <div className="picside">
+                  <div className="pic1">
+                    <img src="http://img-tailor.11222.cn/cover/raw/2015071515141777.jpg" />
+                  </div>
+                  <div className="pic2">
+                    <img src="http://img-tailor.11222.cn/cover/raw/2014082218146282.jpg" />
+                  </div>
+                  <div className="pic3">
+                    <img src="http://img-tailor.11222.cn/cover/raw/2014082218140366.jpg" />
+                  </div>
+                </div>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
